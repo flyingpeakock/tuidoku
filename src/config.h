9158f0338f18eb396b7e6d06be22ce8a1bbe06b9
Philip Johansson <philipphuket@gmail.com>
2021-06-04 22:07:05 +0200

    changed to used define instead of variables to avoid multiple declarations

--- src/config.h
+++ src/config.h
@@ -10,20 +10,20 @@
 
 // Movement keys
 // These keys will work along side the arrow keys
-const char UP_KEY = 'k';
-const char DOWN_KEY = 'j';
-const char LEFT_KEY = 'h';
-const char RIGHT_KEY = 'l';
+#define UP_KEY 'k'
+#define DOWN_KEY 'j'
+#define LEFT_KEY 'h'
+#define RIGHT_KEY 'l'
 
 // Other keys
 
-const char INSERT_KEY = 'i';
-const char PENCIL_KEY = 'p';
-const char QUIT_KEY = 'q';
-const char CHECK_KEY = 'c';
-const char ERASE_KEY = ' ';
-const char GO_KEY = 'g';
-const char TOGGLE_KEY = 27; // escape key. Switches between insert and pencil
+#define INSERT_KEY 'i'
+#define PENCIL_KEY 'p'
+#define QUIT_KEY 'q'
+#define CHECK_KEY 'c'
+#define ERASE_KEY ' '
+#define GO_KEY 'g'
+#define TOGGLE_KEY 27 // escape key. Switches between insert and pencil
 
 // Colors
 
@@ -40,21 +40,21 @@ const char TOGGLE_KEY = 27; // escape key. Switches between insert and pencil
  * To disable any color set the value to -1
  */
 
-const int ERROR_COLOR = COLOR_RED;
-const int CORRECT_COLOR = COLOR_BLUE;
-const int HIGHLIGHT_COLOR = COLOR_YELLOW;
-const int LOWLIGHT_COLOR = COLOR_CYAN;
+#define ERROR_COLOR COLOR_RED
+#define CORRECT_COLOR COLOR_BLUE
+#define HIGHLIGHT_COLOR COLOR_YELLOW
+#define LOWLIGHT_COLOR COLOR_CYAN
 
 // Visual settings
 
-const bool PRINT_TITLE = true;
-const bool PRINT_HELP = true;
-const bool PRINT_COORDS = true;
-const bool PRINT_STATUS = true; // If false time taken to solve a puzzle will not be shown.
+#define PRINT_TITLE true
+#define PRINT_HELP true
+#define PRINT_COORDS true
+#define PRINT_STATUS true // If false time taken to solve a puzzle will not be shown.
 
 // Behavioural settings
 
-const bool START_TIMER = true;
-const bool REMOVE_MARKS = true;
-const bool HIGHLIGHT_SELECTED = true;
-const bool DIM_COMPLETED = true;
+#define START_TIMER true
+#define REMOVE_MARKS true
+#define HIGHLIGHT_SELECTED true
+#define DIM_COMPLETED true
