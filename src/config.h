6923e64d6f97443281adb246c9999ee86bacc453
Philip Johansson <philipphuket@gmail.com>
2021-06-10 11:46:48 +0200

    changed config file to use static consts instead of define

--- src/config.h
+++ src/config.h
@@ -10,7 +10,7 @@
 
 #pragma once
 
-#define TITLE "Tuidoku"
+static const char *TITLE = "Tuidoku";
 
 /*
  * To set a key to a special character such as
@@ -26,81 +26,81 @@
 
 // Movement keys
 // These keys will work along side the arrow keys
-#define UP_KEY 'k'
-#define DOWN_KEY 'j'
-#define LEFT_KEY 'h'
-#define RIGHT_KEY 'l'
+static const char UP_KEY = 'k';
+static const char DOWN_KEY = 'j';
+static const char LEFT_KEY = 'h';
+static const char RIGHT_KEY = 'l';
 
 // Other keys
 
-#define INSERT_KEY 'i'
-#define PENCIL_KEY 'p'
-#define QUIT_KEY 'q'
-#define CHECK_KEY 'c'
-#define ERASE_KEY ' '
-#define GO_KEY 'g'
-#define TOGGLE_KEY 27 // 27 = ESC. Switches between insert and pencil
+static const char INSERT_KEY = 'i';
+static const char PENCIL_KEY = 'p';
+static const char QUIT_KEY = 'q';
+static const char CHECK_KEY = 'c';
+static const char ERASE_KEY = ' ';
+static const char GO_KEY = 'g';
+static const char TOGGLE_KEY = 27; // 27 = ESC. Switches between insert and pencil
 
 // Colors
 
 /* Available colors are:
- *  COLOR_BLACK
- *  COLOR_RED
- *  COLOR_GREEN
- *  COLOR_YELLOW
- *  COLOR_BLUE
- *  COLOR_MAGENTA
- *  COLOR_CYAN
- *  COLOR_WHITE
- * 
+ *  COLOR_BLACK = 0
+ *  COLOR_RED = 1
+ *  COLOR_GREEN = 2
+ *  COLOR_YELLOW = 3
+ *  COLOR_BLUE = 4
+ *  COLOR_MAGENTA = 5
+ *  COLOR_CYAN = 6
+ *  COLOR_WHITE = 7
+ *  DEFAULT = -1
  * To disable any color set the value to -1
  */
 
-#define FOREGROUND_COLOR -1
-#define BACKGROUND_COLOR -1
+static const int FOREGROUND_COLOR = -1;
+static const int BACKGROUND_COLOR = -1;
 
-#define ERROR_COLOR COLOR_RED
-#define CORRECT_COLOR COLOR_BLUE
+static const int ERROR_COLOR = 1;
+static const int CORRECT_COLOR = 4;
 
-#define HIGHLIGHT_COLOR COLOR_YELLOW
-#define LOWLIGHT_COLOR COLOR_CYAN
+static const int HIGHLIGHT_COLOR = 3;
+static const int LOWLIGHT_COLOR = 6;
 
-#define BOARD_COLOR FOREGROUND_COLOR
-#define GIVEN_COLOR FOREGROUND_COLOR
-#define PLACED_COLOR FOREGROUND_COLOR
+static const int BOARD_COLOR = FOREGROUND_COLOR;
+static const int GIVEN_COLOR = FOREGROUND_COLOR;
+static const int PLACED_COLOR = FOREGROUND_COLOR;
 
 // Visual settings
 
-#define PRINT_TITLE true
-#define PRINT_HELP true
-#define PRINT_COORDS true
-#define PRINT_STATUS true // If false time taken to solve a puzzle will not be shown.
-#define ALPHABETICAL_ROW false
-#define ALPHABETICAL_COL false
+static const bool PRINT_TITLE = true;
+static const bool PRINT_HELP = true;
+static const bool PRINT_COORDS = true;
+static const bool PRINT_STATUS = true; // If false time taken to solve a puzzle will not be shown.
+static const bool ALPHABETICAL_ROW = false;
+static const bool ALPHABETICAL_COL = false;
 
 /*
  * Check out https://en.wikipedia.org/wiki/Box-drawing_character#Unicode
  * for other characters that you can use
  */ 
-#define TOPROW "╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗"
-#define ROW1   "║   │   │   ║   │   │   ║   │   │   ║"
-#define ROW2   "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
-#define ROW3   "╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣"
-#define BOTROW "╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝" 
+static const char *TOPROW = "╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗";
+static const char *ROW1   = "║   │   │   ║   │   │   ║   │   │   ║";
+static const char *ROW2   = "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢";
+static const char *ROW3   = "╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣";
+static const char *BOTROW = "╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝";
 
 /*
 // Example of alternate box drawing characters
-#define TOPROW "┏━━━┯━━━┯━━━┳━━━┯━━━┯━━━┳━━━┯━━━┯━━━┓"
-#define ROW1   "┃   │   │   ┃   │   │   ┃   │   │   ┃"
-#define ROW2   "┠───┼───┼───╂───┼───┼───╂───┼───┼───┨"
-#define ROW3   "┣━━━┿━━━┿━━━╋━━━┿━━━┿━━━╋━━━┿━━━┿━━━┫"
-#define BOTROW "┗━━━┷━━━┷━━━┻━━━┷━━━┷━━━┻━━━┷━━━┷━━━┛"
+static const char *TOPROW = "┏━━━┯━━━┯━━━┳━━━┯━━━┯━━━┳━━━┯━━━┯━━━┓";
+static const char *ROW1   = "┃   │   │   ┃   │   │   ┃   │   │   ┃";
+static const char *ROW2   = "┠───┼───┼───╂───┼───┼───╂───┼───┼───┨";
+static const char *ROW3   = "┣━━━┿━━━┿━━━╋━━━┿━━━┿━━━╋━━━┿━━━┿━━━┫";
+static const char *BOTROW = "┗━━━┷━━━┷━━━┻━━━┷━━━┷━━━┻━━━┷━━━┷━━━┛";
 */
 
 
 // Behavioural settings
 
-#define START_TIMER true
-#define REMOVE_MARKS true
-#define HIGHLIGHT_SELECTED true
-#define DIM_COMPLETED true
+static const bool START_TIMER = true;
+static const bool REMOVE_MARKS = true;
+static const bool HIGHLIGHT_SELECTED = true;
+static const bool DIM_COMPLETED = true;
