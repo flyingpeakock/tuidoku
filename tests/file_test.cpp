45d03f620a660f84b8a1380eba1dcf435d9f18f9
Philip Johansson <philipphuket@gmail.com>
2022-12-20 20:15:24 +0100

    added unit test for getSDMpuzzle

--- tests/file_test.cpp
+++ tests/file_test.cpp
@@ -57,3 +57,34 @@ TEST(file_test, getSDKPuzzle) {
         EXPECT_EQ(puzzle[0].getPlayGrid(), buildPuzzle(test.puzzle));
     }
 }
+
+/**
+ * @brief Unit test for getSDMpuzzle wich parses SDM files for puzzles
+ * 
+ */
+TEST(file_test, getSDMpuzzle) {
+    file_test_t test_table[] = {
+        {
+            TEST_PUZZLES_ROOT_DIR "sdmpuzzle1.sdm",
+            {
+                {0, 1, 6, 4, 0, 0, 0, 0, 0},
+                {2, 0, 0, 0, 0, 9, 0, 0, 0},
+                {4, 0, 0, 0, 0, 0, 0, 6, 2},
+                {0, 7, 0, 2, 3, 0, 1, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 3},
+                {0, 0, 3, 0, 8, 7, 0, 4, 0},
+                {9, 6, 0, 0, 0, 0, 0, 0, 5},
+                {0, 0, 0, 8, 0, 0, 0, 0, 7},
+                {0, 0, 0, 0, 0, 6, 8, 2, 0}
+            }
+        },
+    };
+
+    for (auto &test : test_table) {
+        std::ifstream file;
+        file.open(test.fileName);
+        auto puzzle = file::getSDMPuzzle(file);
+        ASSERT_GT(puzzle.size(), 0);
+        EXPECT_EQ(puzzle[0].getPlayGrid(), buildPuzzle(test.puzzle));
+    }
+}
