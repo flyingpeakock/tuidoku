cd57dce1f3e85cc79924edab50437c6c24895a06
Philip Johansson <philipphuket@gmail.com>
2022-12-21 10:02:06 +0100

    added unit test for findUniqueRectangle

--- tests/humanSolve_test.cpp
+++ tests/humanSolve_test.cpp
@@ -6,16 +6,9 @@
  * 
  */
 TEST(HumanSolve_test, findNakedSingle) {
-    struct findNakedSingle_t {
-        std::uint16_t num;
-        Move move;
-        bool ret;
-        const char *gridString;
-    };
-
-    findNakedSingle_t test_table[] = {
-        {(1 << 7), {'8', 3, 2, &Board::insert}, true, "200150074001000020407600013040200095070480060010000430706000289004907350003000040"},
-        {(1 << 5), {'6', 5, 6, &Board::insert}, true, "412736589000000106568010370000050210100000008087090000030070865800000000000908401"},
+    single_digit_human_solve_t test_table[] = {
+        {(1 << 7), {'8', 3, 2, &Board::insert}, 0, true, "200150074001000020407600013040200095070480060010000430706000289004907350003000040"},
+        {(1 << 5), {'6', 5, 6, &Board::insert}, 0, true, "412736589000000106568010370000050210100000008087090000030070865800000000000908401"},
     };
 
     for (auto &test : test_table) {
@@ -39,15 +32,8 @@ TEST(HumanSolve_test, findNakedSingle) {
  * 
  */
 TEST(HumanSolve_test, findHiddenSingle) {
-    struct findHiddenSingle_t {
-        std::uint16_t num;
-        Move move;
-        bool ret;
-        const char *gridString;
-    };
-
-    findHiddenSingle_t test_table[] = {
-        {(1 << 0), {'1', 6, 2, &Board::insert}, true, "009032000000700000162000000010020560000900000050000107000000403026009000005870000"},
+    single_digit_human_solve_t test_table[] = {
+        {(1 << 0), {'1', 6, 2, &Board::insert}, 0, true, "009032000000700000162000000010020560000900000050000107000000403026009000005870000"},
     };
 
     for (auto &test : test_table) {
@@ -67,15 +53,7 @@ TEST(HumanSolve_test, findHiddenSingle) {
 }
 
 TEST(HumanSolve_test, findXwing) {
-    struct findXwing_t {
-        std::uint16_t num;
-        Move move;
-        int moves;
-        bool ret;
-        const char *gridString;
-    };
-
-    findXwing_t test_table[] = {
+    single_digit_human_solve_t test_table[] = {
         {(1 << 4), {'5', 3, 4, &Board::pencil}, 1, true, "041729030769003402032640719403900170607004903195370024214567398376090541958431267"},
         {(1 << 0), {'1', 1, 3, &Board::pencil}, 9, true, "980062753065003000327050006790030500050009000832045009673591428249087005518020007"},
     };
@@ -89,6 +67,28 @@ TEST(HumanSolve_test, findXwing) {
         std::stringstream boardstrstream;
         board.printBoard(boardstrstream);
         EXPECT_EQ(findXwing(board, test.num, moves), test.ret) << "Could not find X-Wing:\n" << boardstrstream.str();
+        if (test.ret == false) continue;
+
+        ASSERT_EQ(moves.size(), test.moves);
+        EXPECT_EQ(moves[0].val, test.move.val);
+        EXPECT_EQ(moves[0].row, test.move.row);
+        EXPECT_EQ(moves[0].col, test.move.col);
+        EXPECT_EQ(moves[0].move, test.move.move);
+    }
+}
+
+TEST(HumanSolve_test, findUniqueRectangle) {
+    single_digit_human_solve_t test_table[] = {
+        {((1 << 7) | (1 << 8)), {'8', 1, 1, &Board::pencil}, 2, true, "502008967100700452067500381213657849654891273700004615821900034306000098005083026"},
+    };
+
+    for (auto &test : test_table) {
+        std::vector<Move> moves;
+        Generator gen(test.gridString);
+        Board board = gen.createBoard();
+        board.autoPencil();
+
+        EXPECT_EQ(findUniqueRectangle(board, test.num, moves), test.ret);
         if (test.ret == false) continue;
 
         ASSERT_EQ(moves.size(), test.moves);
--- tests/humanSolve_test.h
+++ tests/humanSolve_test.h
@@ -2,3 +2,11 @@
 
 #include "../src/HumanSolve.h"
 #include "../src/Generator.h"
+
+struct single_digit_human_solve_t {
+    std::uint16_t num;
+    Move move;
+    int moves;
+    bool ret;
+    const char *gridString;
+};
