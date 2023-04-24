0eacbc96f72538a388eba4a16d390ae65b3099e3
Philip Johansson <philipphuket@gmail.com>
2023-04-24 12:58:38 +0200

    added functions for searching for mistakes

---
+++ src/Sudoku/Logic.cpp
@@ -0,0 +1,107 @@
+#include "Logic.h"
+
+/**
+ * @brief vector containing all the methods used to find wrong moves
+ * 
+ */
+static std::vector<bool (*)(const Sudoku::SudokuPuzzle &puzzle, Sudoku::logic::Move &move)> mistakes = {
+    &Sudoku::logic::foundIllogicalInput,
+    &Sudoku::logic::foundIllogicalPencil,
+    &Sudoku::logic::foundWrongRemovedPencil,
+    &Sudoku::logic::foundMissingPencilMark,
+    &Sudoku::logic::foundWrongInput,
+};
+
+bool Sudoku::logic::foundIllogicalInput(const SudokuPuzzle &puzzle, Move &move) {
+    for (const auto &i : puzzle.wrong_inputs) {
+        move.type = eLogicErrorInsert;
+        move.diff = eBeginner;
+        move.falses.push_back(i);
+        return true;
+    }
+    return false;
+}
+
+bool Sudoku::logic::foundIllogicalPencil(const SudokuPuzzle &puzzle, Move &move) {
+    for (const auto &i : puzzle.wrong_marks) {
+        move.type = eLogicErrorPencil;
+        move.diff = eBeginner;
+        move.falses.push_back(i);
+        return true;
+    }
+    return false;
+}
+
+bool Sudoku::logic::foundWrongRemovedPencil(const SudokuPuzzle &puzzle, Move &move) {
+    for (const auto &i : puzzle.removed_marks) {
+        for (const auto &solution : puzzle.constraintTable->solution) {
+            if (i->count == solution->count) {
+                move.type = eLogicErrorInsert;
+                move.diff = eBeginner;
+                move.falses.push_back(i);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool Sudoku::logic::foundMissingPencilMark(const SudokuPuzzle &puzzle, Move &move) {
+    for (auto col = puzzle.constraintTable->root.right; col != &puzzle.constraintTable->root; col = col->right) {
+        for (auto row = col->down; row != col; row = row->down) {
+            bool foundInMarks = false;
+            for (auto i = puzzle.pencilMarks.begin(); i < puzzle.pencilMarks.end(); i++) {
+                if (row->count == (*i)->count) {
+                    foundInMarks = true;
+                    break;
+                }
+            }
+            if (!foundInMarks) {
+                move.type = eLogicErrorPencilMissing;
+                move.diff = eBeginner;
+                move.falses.push_back(row);
+                return true;
+            }
+
+        }
+    }
+    return false;
+}
+
+bool Sudoku::logic::foundWrongInput(const SudokuPuzzle &puzzle, Move &move) {
+    for (auto i = puzzle.constraintTable->current.begin() + puzzle.current_start_index; i < puzzle.constraintTable->current.end(); i++) {
+        bool isInSolution = false;
+        for (auto j = puzzle.constraintTable->solution.begin(); j < puzzle.constraintTable->solution.end(); j++) {
+            if ((*j)->count == (*i)->count) {
+                isInSolution = true;
+                break;
+            }
+        }
+        if (!isInSolution) {
+            move.type = eLogicErrorInsert;
+            move.diff = eBeginner;
+            move.falses.push_back(*i);
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Sudoku::logic::foundMistake(const SudokuPuzzle &puzzle, Move &move) {
+    for (auto &method : mistakes) {
+        if (method(puzzle, move)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Sudoku::logic::Move Sudoku::logic::getNextMove(const Sudoku::SudokuPuzzle &puzzle, bool &foundMove) {
+    Move move;
+    foundMove = false;
+    if (foundMistake(puzzle, move)) {
+        foundMove = true;
+        return move;
+    }
+    return move;
+}
--- src/Sudoku/Logic.h
+++ src/Sudoku/Logic.h
@@ -17,9 +17,9 @@ namespace Sudoku {
          * @brief move containig vector of truths and falses, move type and difficulty
          * 
          */
-        struct move {
+        struct Move {
             moveType type;
-            difficulty difficulty;
+            difficulty diff;
             /**
              * @brief Vector contaiing truths found using logic
              * 
@@ -37,5 +37,31 @@ namespace Sudoku {
              */
             std::vector<DancingLink *> falses;
         };
+
+        // Mistakes
+        bool foundIllogicalInput(const SudokuPuzzle &puzzle, Move &move);
+        bool foundIllogicalPencil(const SudokuPuzzle &puzzle, Move &move);
+        bool foundWrongRemovedPencil(const SudokuPuzzle &puzzle, Move &move);
+        bool foundMissingPencilMark(const SudokuPuzzle &puzzle, Move &move);
+        bool foundWrongInput(const SudokuPuzzle &puzzle, Move &move);
+
+        /**
+         * @brief Searches for any mistakes in the board
+         * 
+         * @param puzzle puzzle to search through
+         * @param move move which gets set if a mistake is found
+         * @return true if a mistake is found
+         * @return false if no mistkes are found
+         */
+        bool foundMistake(const SudokuPuzzle &puzzle, Move &move);
+
+        /**
+         * @brief Get the Next Move found in puzzle
+         * 
+         * @param puzzle SubokuPuzzle object
+         * @param foundMove gets set true if a move if found, otherwise sets to false
+         * @return Move move object
+         */
+        Move getNextMove(const SudokuPuzzle &puzzle, bool &foundMove);
     }
 }
