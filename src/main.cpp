9f678729e7bd3f05159a4453fbfacd4cd883e502
Philip Johansson <philipphuket@gmail.com>
2023-05-16 14:38:28 +0200

    starting config

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "Tui/Tui.h"                    // for Tui
 #include "sudoku/DancingLinkObjects.h"  // for eSize, DancingLinkContainer
 
+#include <libconfig.h++>
+
 int main(int argc, char *argv[]) {
+
+    libconfig::Config cfg;
+
     tui::Tui tui;
     tui.runLoop();
     return 0;
